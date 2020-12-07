3d649949883db1b9a5f0747f2758f615d5d0c338
jamie120 <jamie120@hotmail.com>
2020-12-07 12:08:32 +0000

    add jquery effects to animate service finder

--- assets/js/service-helper.js
+++ assets/js/service-helper.js
@@ -1,20 +1,86 @@
 $(document).ready(function(){
 
+    var timedMaster = 
+    `<h1 class='text-box-style hide-on-append' id='how-long-timed-master'>Maybe you could benefit from our timed mastering service. How long is your mix/podcast?</h1>
+    <input class='text-box-style hide-on-append' id='minutes-input' type='text' placeholder='length in minutes'>
+    <button class='text-box-style hide-on-append' id='calc-timed-master'>CALCULATE</button>`
+
+    var requireMixing = 
+    `<div class="row row__A>
+        <div class="col">
+            <h1 class='text-box-style' id='happy-with-mix'>Are you happy with the mix-down of the track?</h1>
+        </div>
+    </div>
+    <div class="row row__A>
+        <div class="col">
+            <button class='button-option text-box-style' id='yes-mix-btn'>YES</button>
+            <button class='button-option text-box-style' id='no-mix-btn'>NO</button>
+        </div>
+    </div>`
+
+    var standardMastering = 
+    `<div class="row">
+        <div class="col">
+            <h2 class='text-box-style' id="standard-mastering-suggestion">Our Standard Mastering service may be suited to your project. </h2></div></div>
+    <div class="row">
+        <div class="col">
+            <div id="standard-master-calc"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h3 class='text-box-style' id="priority-master">Priority Service (3 day turnaround) </h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <button class='button-option text-box-style' id='yes-priority-btn'>YES</button>
+        </div>
+        <div class="col d-flex justify-content-center">
+            <button class='button-option text-box-style' id='no-priority-btn'>NO</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h3 class='text-box-style' id="vinyl-master">Vinyl Master </h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <button class='button-option text-box-style' id='yes-vinyl-btn'>YES</button>
+        </div>
+        <div class="col d-flex justify-content-center">
+            <button class='button-option text-box-style' id='no-vinyl-btn'>NO</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <a href="mastering.html"><button class='text-box-style'>GOTO OUR MASTERING SERVICES PAGE</button></a>
+        </div>
+    </div>`
+
+    // https://stackoverflow.com/questions/17883692/how-to-set-time-delay-in-javascript  -SUPPORT FOR SETTING DELAY TAKEN FROM HERE
 
     $("#dj-btn").click(function() {
-        $("#question").html("<h1>You have selected DJ MIX</h1>");
-        $(".button-option").css("display","none");
-        $(".row__A").css("display","none");
-        $(".row__B").css("display","none");
-        $("#service-text-container").append(timedMaster);
+        fadeIcons();
+        var delayInMilliseconds = 1000; //1 second
+        setTimeout(function() {
+            $("#question").html("<h1>You have selected DJ MIX</h1>");
+            $(".button-option").css("display","none");
+            $("#service-text-container").append(timedMaster);
+            fadeInAllAfterFirst();
+        }, delayInMilliseconds);
     });
 
     $("#podcast-btn").click(function() {
-        $("#question").html("<h1>You have selected PODCAST</h1>");
-        $(".button-option").css("display","none");
-        $(".row__A").css("display","none");
-        $(".row__B").css("display","none");
-        $("#service-text-container").append(timedMaster);
+        fadeIcons();
+        var delayInMilliseconds = 1000; //1 second
+        setTimeout(function() {
+            $("#question").html("<h1>You have selected PODCAST</h1>");
+            $(".button-option").css("display","none");
+            $("#service-text-container").append(timedMaster);
+            fadeInAllAfterFirst();
+        }, delayInMilliseconds);
     });
 
     $(document).on('click', '#calc-timed-master', function(){
@@ -90,14 +156,7 @@ $(document).ready(function(){
         $("#stem-mastering-suggestion").css("display","none");
         $("#question").css("display","none");
         $("#service-finder-content").append(standardMastering);
-        //$("#service-text-container").append(`<h2 class='text-box-style' id="standard-mastering-suggestion">Our Standard Mastering service may be suited to your project. </h2>`);
-        //$("#service-text-container").append(`<h2 class='text-box-style'>Do you require the following? </h2>`);
-        //$("#service-text-container").append(`<h3 class='text-box-style' id="priority-master">Priority Service (3 day turnaround) </h3> <hr>`);
-        //$("#service-text-container").append("<button class='button-option text-box-style' id='yes-priority-btn'>YES</button>");
-        //$("#service-text-container").append("<button class='button-option text-box-style' id='no-priority-btn'>NO</button>");
-        //$("#service-text-container").append(`<h3 class='text-box-style' id="vinyl-master">Vinyl Master </h3>`);
-        //$("#service-text-container").append("<button class='button-option text-box-style' id='yes-vinyl-btn'>YES</button>");
-        //$("#service-text-container").append("<button class='button-option text-box-style' id='no-vinyl-btn'>NO</button>");
+        checkSelector(vinyl, priority)
     });
 
     $(document).on('click', '#calc-stem-master', function(){
@@ -121,60 +180,64 @@ $(document).ready(function(){
         $("#calc-mix").css("display","none");
         $("#service-text-container").append(`<h2 class='text-box-style' >You mixing is estimated to cost: £${price} based on ${tracks} number of tracks provided by you </h2>`);
     });
+
+    //CALCULATE MASTER LOGIC //
+
+    var vinyl = false
+    var priority = false
+    var price = 25
+
+    $(document).on('click', '#yes-vinyl-btn', function(){
+        vinyl = true
+        $("#standard-mastering-calc").html("");
+        checkSelector(vinyl, priority)
+    });
+
+    $(document).on('click', '#no-vinyl-btn', function(){
+        vinyl = false
+        $("#standard-mastering-calc").html("");
+        checkSelector(vinyl, priority)
+    });
+
+    $(document).on('click', '#yes-priority-btn', function(){
+        priority = true
+        checkSelector(vinyl, priority)
+    });
+
+    $(document).on('click', '#no-priority-btn', function(){
+        priority = false
+        checkSelector(vinyl, priority)
+    });
+    
+    function checkSelector(vinyl, priority) {
+        if (vinyl == true && priority == true) {
+            $("#standard-master-calc").html("")
+            $("#standard-master-calc").append(`<h2 class='text-box-style' id="standard-mastering-calc" >You mastering is estimated to cost: £${price + 30}. This price is per track, with vinyl and priority services attached </h2>`);
+        } else if (vinyl == true && priority == false) {
+            $("#standard-master-calc").html("")
+            $("#standard-master-calc").append(`<h2 class='text-box-style' id="standard-mastering-calc" >You mastering is estimated to cost: £${price + 5}. This price is per track, with the vinyl mastering service. </h2>`);
+        } else if (vinyl == false && priority == true) {
+            $("#standard-master-calc").html("")
+            $("#standard-master-calc").append(`<h2 class='text-box-style' id="standard-mastering-calc" >You mastering is estimated to cost: £${price + 25}. This price is per track, with the priority mastering service. </h2>`);
+        } else {
+            $("#standard-master-calc").html("")
+            $("#standard-master-calc").append(`<h2 class='text-box-style' id="standard-mastering-calc" >You mastering is estimated to cost: £${price}. You can add 'vinyl" & 'priority' services by selecting them below for an updated estimate.`);
+        }
+    };
+
+    //ANIMATION LOGIC //
+
+    function fadeInAllAfterFirst() {
+        $( ".hide-on-append:hidden" ).first().fadeIn( 800, function() {
+            $( ".hide-on-append:hidden" ).fadeIn( 600 );
+        });
+    }
+
+    function fadeIcons() {
+        $(".row__A").fadeToggle( 1000, "linear" );
+        $(".row__B").fadeToggle( 1000, "linear" );
+    }
+
 });
 
 
-var standardMastering = 
-`<div class="row">
-    <div class="col">
-        <h2 class='text-box-style' id="standard-mastering-suggestion">Our Standard Mastering service may be suited to your project. </h2></div></div>
-<div class="row">
-    <div class="col">
-        <h2 class='text-box-style'>Do you require the following? </h2>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
-        <h3 class='text-box-style' id="priority-master">Priority Service (3 day turnaround) </h3>
-    </div>
-</div>
-<div class="row">
-    <div class="col d-flex justify-content-center">
-        <button class='button-option text-box-style' id='yes-priority-btn'>YES</button>
-    </div>
-    <div class="col">
-        <button class='button-option text-box-style' id='no-priority-btn'>NO</button>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
-        <h3 class='text-box-style' id="vinyl-master">Vinyl Master </h3>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
-        <button class='button-option text-box-style' id='yes-vinyl-btn'>YES</button>
-    </div>
-    <div class="col">
-        <button class='button-option text-box-style' id='no-vinyl-btn'>NO</button>
-    </div>
-</div>`
-
-
-var timedMaster = 
-`<h1 class='text-box-style' id='how-long-timed-master'>Maybe you could benefit from our timed mastering service. How long is your mix/podcast?</h1>
-<input class='text-box-style' id='minutes-input' type='text' placeholder='length in minutes'>
-<button class='text-box-style' id='calc-timed-master'>CALCULATE</button>`
-
-var requireMixing = 
-`<div class="row row__A>
-    <div class="col">
-        <h1 class='text-box-style' id='happy-with-mix'>Are you happy with the mix-down of the track?</h1>
-    </div>
-</div>
-<div class="row row__A>
-    <div class="col">
-        <button class='button-option text-box-style' id='yes-mix-btn'>YES</button>
-        <button class='button-option text-box-style' id='no-mix-btn'>NO</button>
-    </div>
-</div>`
