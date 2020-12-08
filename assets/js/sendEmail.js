559b6f120fa50288029ae9a06436e09acc9e48aa
jamie120 <jamie120@hotmail.com>
2020-12-08 15:21:43 +0000

    add contact us page and email response

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,27 @@
-console.log("Helo")
-
 function sendMail(contactForm) {
-    console.log(contactForm.email.value)
     emailjs.send("gmail", "joinMailList", {
         from_email: contactForm.email.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    $('#modalJoinMail').modal('hide');
+    $('#modalJoinSuccess').modal('show');
+    return false;
+}
+
+function sendCopyMail(contactForm) {
+    alert("triggered")
+    emailjs.send("gmail", "contactForm", {
+        from_name: contactForm.name.value,
+        from_email: contactForm.email.value,
+        from_subject: contactForm.subject.value,
+        from_message: contactForm.message.value,
     })
     .then(
         function(response) {
