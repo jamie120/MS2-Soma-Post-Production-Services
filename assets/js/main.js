ec72031ab749c9659b51586090b30117feb56f45
jamie120 <jamie120@hotmail.com>
2020-12-10 12:43:11 +0000

    add service keypoints element to html, style responsively

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@
 $("#mastering-prices").change(function(){
      var status = this.value;
    if(status=="standard") {
-     $('#price-box').text("standard pricing");
+     $('#price-box-text').text("standard pricing");
    } else if(status=="priority") {
-     $('#price-box').text("priority pricing");
+     $('#price-box-text').text("priority pricing");
    } else if(status=="vinyl") {
-     $('#price-box').text("vinyl pricing");
+     $('#price-box-text').text("vinyl pricing");
    };
 });
 
@@ -19,11 +19,11 @@ $("#mastering-prices").change(function(){
 $("#stem-mastering-prices").change(function(){
      var status = this.value;
    if(status=="1-4") {
-     $('#price-box').text("1-4 pricing");
+     $('#price-box-text').text("1-4 pricing");
    } else if(status=="5-8") {
-     $('#price-box').text("5-8 pricing");
+     $('#price-box-text').text("5-8 pricing");
    } else if(status=="9-12") {
-     $('#price-box').text("9-12 pricing");
+     $('#price-box-text').text("9-12 pricing");
    };
 });
 
@@ -32,17 +32,17 @@ $("#stem-mastering-prices").change(function(){
 $("#mixing-prices").change(function(){
      var status = this.value;
    if(status=="1-10") {
-     $('#price-box').text("From £54 per track");
+     $('#price-box-text').text("From £54 per track");
    } else if(status=="11-20") {
-     $('#price-box').text("From £72 per track");
+     $('#price-box-text').text("From £72 per track");
    } else if(status=="21-30") {
-     $('#price-box').text("From £90 per track");
+     $('#price-box-text').text("From £90 per track");
    } else if(status=="31-40") {
-     $('#price-box').text("From £108 per track");
+     $('#price-box-text').text("From £108 per track");
    } else if(status=="41-50") {
-     $('#price-box').text("From £126 per track");
+     $('#price-box-text').text("From £126 per track");
    } else if(status=="51-60") {
-     $('#price-box').text("From £144 per track");
+     $('#price-box-text').text("From £144 per track");
    };
 });
 
