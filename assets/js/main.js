84799e6d360e9034f1098e49e65fff32e66ab6c0
jamie120 <jamie120@hotmail.com>
2020-12-14 16:37:51 +0000

    style price box and select menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@
 $("#mastering-prices").change(function(){
      var status = this.value;
    if(status=="standard") {
-     $('#price-box-text').text("standard pricing");
+     $('#price-box-text').text("£25");
    } else if(status=="priority") {
-     $('#price-box-text').text("priority pricing");
+     $('#price-box-text').text("£50");
    } else if(status=="vinyl") {
-     $('#price-box-text').text("vinyl pricing");
+     $('#price-box-text').text("£30");
    };
 });
 
@@ -21,11 +21,11 @@ $("#mastering-prices").chosen({disable_search_threshold: 10});
 $("#stem-mastering-prices").change(function(){
      var status = this.value;
    if(status=="1-4") {
-     $('#price-box-text').text("1-4 pricing");
+     $('#price-box-text').text("£40");
    } else if(status=="5-8") {
-     $('#price-box-text').text("5-8 pricing");
+     $('#price-box-text').text("£50");
    } else if(status=="9-12") {
-     $('#price-box-text').text("9-12 pricing");
+     $('#price-box-text').text("£60");
    };
 });
 
@@ -36,17 +36,17 @@ $("#stem-mastering-prices").chosen({disable_search_threshold: 10});
 $("#mixing-prices").change(function(){
      var status = this.value;
    if(status=="1-10") {
-     $('#price-box-text').text("From £54 per track");
+     $('#price-box-text').text("£60");
    } else if(status=="11-20") {
-     $('#price-box-text').text("From £72 per track");
+     $('#price-box-text').text("£80");
    } else if(status=="21-30") {
-     $('#price-box-text').text("From £90 per track");
+     $('#price-box-text').text("£100");
    } else if(status=="31-40") {
-     $('#price-box-text').text("From £108 per track");
+     $('#price-box-text').text("£120");
    } else if(status=="41-50") {
-     $('#price-box-text').text("From £126 per track");
+     $('#price-box-text').text("£140");
    } else if(status=="51-60") {
-     $('#price-box-text').text("From £144 per track");
+     $('#price-box-text').text("£160");
    };
 });
 
