371fd910ccb9dd6f7f8822157dd2a3a237144909
jamie120 <jamie120@hotmail.com>
2020-12-20 15:06:41 +0000

    add notes to email.js

--- assets/js/emailJs.js
+++ assets/js/emailJs.js
@@ -1,7 +1,13 @@
+/** Initialise emailJS */
+
 (function(){
             emailjs.init("user_ay2av5pwPBy3wqNOgCBCc");
         })();
 
+
+/** Function to send email template 'joinMailList', created using emailJs.
+ * Once the email is sent, the modal containing the subscribe button is hidden and the success modal (#modalJoinSuccess) is displayed on the screen.
+ */       
 function sendMail(contactForm) {
     emailjs.send("gmail", "joinMailList", {
         from_email: contactForm.email.value,
@@ -19,6 +25,9 @@ function sendMail(contactForm) {
     return false;
 }
 
+/** Function to send email template 'contactForm', created using emailJs.
+ * Once the email is sent, a confirmation modal (#modalContactSubmit) is displayed on the screen.
+ */       
 function sendCopyMail(contactForm) {
     emailjs.send("gmail", "contactForm", {
         from_name: contactForm.name.value,
